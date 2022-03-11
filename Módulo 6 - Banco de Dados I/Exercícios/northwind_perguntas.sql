a976523b0527ac19ba74c9755f4b1d19e3422d2d
joaopasini <joao_pasini@hotmail.com>
2022-03-10 23:59:10 -0300

    northwind perguntas

--- Módulo 6 - Banco de Dados I/Exercícios/northwind_perguntas.sql
+++ Módulo 6 - Banco de Dados I/Exercícios/northwind_perguntas.sql
@@ -4,7 +4,7 @@ Informações: SELECT * FROM orders; SELECT * FROM order_details; SELECT * FROM 
 
 SUM((products.unit_price * order_details.quantity * order_details.discount)) + AVG(orders.freight) AS valor_total Em alguns momentos vocês vão usar:
 
-SUM((products.unit_price * order_details.quantity * (1 - order_details.discount))) + AVG(orders.freight) as total_order_price Em algum momento vocês vão usar:
+SUM((products.unit_price * order_details.quantity * (1 - order_details.discount)) + AVG(orders.freight) as total_order_price Em algum momento vocês vão usar:
 
 ROUND( SUM((products.unit_price * order_details.quantity * (1 - order_details.discount))) + AVG(orders.freight) ) as total_order_price Em algum momento vocês vão usar:
 
@@ -12,20 +12,26 @@ ROUND( SUM((products.unit_price * order_details.quantity * (1 - order_details.di
 
 Obtenha uma tabela que contenha o id do pedido e o valor total do mesmo.
 
-SELECT order_id, unit_price
-FROM order_details ;
-
+SELECT orders.order_id, SUM((products.unit_price * order_details.quantity * (1- order_details.discount) + AVG(orders.freight)) AS valor_total,
+FROM orders, order_details, products,
+GROUP BY 1;	
+	
 Obtenha uma lista dos 10 clientes que realizaram o maior número de pedidos, bem como o número de pedidos de cada, ordenados em ordem decrescente de nº de pedidos.
 
 
 
 Obtenha uma tabela que contenha o id e o valor total do pedido e o nome do cliente que o realizou.
 
-SELECT quantity,SUM(unit_price)
-FROM order_details GROUP BY 1;
+	
+SELECT contact_name, customer_id, SUM((products.unit_price * order_details.quantity * (1- order_details.discount)) + AVG(orders.freight)
+FROM customers, order_details, products
+									 ;
+
 
 Obtenha uma tabela que contenha o país do cliente e o valor da compra que ele realizou.
 
+SELECT contact_name, country, quantity*(unit_price) AS total_purchase
+FROM customers, order_details;
 
 
 Obtenha uma tabela que contenha uma lista dos países dos clientes e o valor total de compras realizadas em cada um dos países. Ordene a tabela, na order descrescente, considerando o valor total de compras realizadas por país.
@@ -33,8 +39,6 @@ Obtenha uma tabela que contenha uma lista dos países dos clientes e o valor tot
 
 
 Obtenha uma tabela com o valor médio das vendas em cada mês (ordenados do mês com mais vendas para o mês com menos vendas).
-
-
 
 
 
